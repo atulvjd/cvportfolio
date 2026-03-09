bb4863c85326a9501d16dab28d7b6cc714ade1dd
atulvjd <atul.sahu025@gmail.com>
2026-03-09 16:29:24 +0530

    P7 N8

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ['@sparticuz/chromium-min', 'puppeteer-core', 'lighthouse'],
 };
 
 export default nextConfig;
